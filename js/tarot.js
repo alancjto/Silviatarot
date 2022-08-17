7bdb48af65f03e36a67187fd22ae85f1a31073b2
alancjto <alan_jjjj@hotmail.com>
2022-08-16 21:02:09 -0300

    corrigiendo eventos y interaccion

--- js/tarot.js
+++ js/tarot.js
@@ -1,99 +1,102 @@
-// Exijo al usuario a enviar "XALAN" sino vuelvo a pedirle un dato 
-
-const { access } = require("graceful-fs");
-const { identityForEmail } = require("sshpk");
-
-let usuario = prompt("Porfavor ingrese su nombre de usuario"); 
-
-while (usuario != "XALAN"){
-    console.log("EL USUARIO INGRESO " + usuario); 
-    usuario = prompt("!Usuario Incorrecto !");
-}
-
-if(usuario == "XALAN"){
-    console.log("XALAN INGRESO");
-    alert("Bienvenido de nuevos XALAN");
-}
+// // Exijo al usuario a enviar "XALAN" sino vuelvo a pedirle un dato 
 
 
-// Decalro funcion para el promedio de 5 notas //
+// let usuario = prompt("Porfavor ingrese su nombre de usuario"); 
 
-function promedioNotas(nota1, nota2 , nota3, nota4, nota5){
-    let resultadoSuma = nota1 + nota2 + nota3 + nota4 + nota5;
-    let promedio = resultadoSuma / 5;
-    return promedio;
+// while (usuario != "XALAN"){
+//     console.log("EL USUARIO INGRESO " + usuario); 
+//     usuario = prompt("!Usuario Incorrecto !");
+// }
 
-}
+// if(usuario == "XALAN"){
+//     console.log("XALAN INGRESO");
+//     alert("Bienvenido de nuevos XALAN");
+// }
 
 
-let nota1 = parseInt(prompt("ingrese la primer nota"));
-let nota2 = parseInt(prompt("ingrese la segunda nota"));
-let nota3 = parseInt(prompt("ingrese la tercera nota"));
-let nota4 = parseInt(prompt("ingrese la cuarta nota"));
-let nota5 = parseInt(prompt("ingrese la quinta nota"));
+// // Decalro funcion para el promedio de 5 notas //
 
-let respuesta = promedioNotas(nota1 , nota2 , nota3 , nota4 , nota5);
-alert("TU PROMEDIO ES " + respuesta);
+// function promedioNotas(nota1, nota2 , nota3, nota4, nota5){
+//     let resultadoSuma = nota1 + nota2 + nota3 + nota4 + nota5;
+//     let promedio = resultadoSuma / 5;
+//     return promedio;
 
-// meto las notas en un array // 
+// }
 
-const arrayNotas = [];
-arrayNotas.unshift(nota1, nota2, nota3, nota4 ,nota5);
-console.log(arrayNotas);
+
+// let nota1 = parseInt(prompt("ingrese la primer nota"));
+// let nota2 = parseInt(prompt("ingrese la segunda nota"));
+// let nota3 = parseInt(prompt("ingrese la tercera nota"));
+// let nota4 = parseInt(prompt("ingrese la cuarta nota"));
+// let nota5 = parseInt(prompt("ingrese la quinta nota"));
+
+// let respuesta = promedioNotas(nota1 , nota2 , nota3 , nota4 , nota5);
+// alert("TU PROMEDIO ES " + respuesta);
+
+// // meto las notas en un array // 
+
+// const arrayNotas = [];
+// arrayNotas.unshift(nota1, nota2, nota3, nota4 ,nota5);
+// console.log(arrayNotas);
 
 
 
-// clase domm // 
 
-const productos = [
-    {
-        nombre: "vodka",
-        precio: 2500,
-    },
-{
-        nombre: "mesa" ,
-        precio: 23800,
-    },
-];
-
-const productoHTNL = (producto) => {
-    let texto = "<div>";
-    texto += "<h1>" + producto.nombre + "</h1>";
-    texto += "<p>" + producto.precio + "</p>";
-    texto += "</div>";
-
-    return texto;
-};
-
-let catalogoHTML= "";
-
-for(let i = 0; i < productos.length; i++) {
-    catalogoHTML += productoHTNL(productos[i]);
-}
 
 // creando eventos con imput // 
 
-const imput = document.getElementById("Nombre y Apellido")
+const imput = document.getElementById('Nombre y Apellido')
 
-<input placeholder="Nombre y Apellido" type="text" tabindex="1" required autofocus></imput> 
+const email = document.getElementById('email')
 
-const accion = () => {
-    console.log('Has hecho click') 
+const tel = document.getElementById('phone')
+
+const descargo = document.getElementById('mensaje')
+
+const accion1 = () => {
+    console.log('Has hecho click')
 }
 
-imput.addEventListener('click' , accion);
-
-imput.addEventListener('imput' , (event) => {
-    console.log(event.data)
-}) 
-
-let buton = document.getElementById('submit')
-
-buton.addEventListener('click' , funcionALlamar);
-
-function funcionALlamar () {
-    console.log("mensaje");
+const accion2 = () => {
+    console.log('Ingresando mail')
 }
+
+const accion3 = () => {
+    console.log('discando numero')
+}
+
+const accion4 = () => {
+    console.log('dejando comentario')
+}
+
+
+imput.addEventListener('click' , accion1)
+
+email.addEventListener('click' , accion2)
+
+tel.addEventListener('click' , accion3)
+
+descargo.addEventListener('click' , accion4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
