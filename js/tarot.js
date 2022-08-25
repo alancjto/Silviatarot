00fbc1e6e6d2bb167c4b9bf81dc55d090d888ecb
alancjto <alan_jjjj@hotmail.com>
2022-08-25 18:21:39 -0300

    segunda entrega final

--- js/tarot.js
+++ js/tarot.js
@@ -1,82 +1,74 @@
-// // Exijo al usuario a enviar "XALAN" sino vuelvo a pedirle un dato 
+// creando eventos con imput y agregando datos del imput generados por los usuarios al local storage y viendolos en consola con json.// 
+
+if (!localStorage.getItem('formValues')) {
+    formValues = {};
+    
+} else {
+    formValues = localStorage.getItem('formValues');
+    
+}
 
 
-// let usuario = prompt("Porfavor ingrese su nombre de usuario"); 
+const Nombre = document.getElementById('validationCustom01')
 
-// while (usuario != "XALAN"){
-//     console.log("EL USUARIO INGRESO " + usuario); 
-//     usuario = prompt("!Usuario Incorrecto !");
-// }
+const Apellido = document.getElementById('validationCustom02')
 
-// if(usuario == "XALAN"){
-//     console.log("XALAN INGRESO");
-//     alert("Bienvenido de nuevos XALAN");
-// }
+const Email = document.getElementById('validationCustomUsername')
 
+const Ciudad = document.getElementById('validationCustom03')
 
-// // Decalro funcion para el promedio de 5 notas //
+const Telefono = document.getElementById('validationCustom05')
 
-// function promedioNotas(nota1, nota2 , nota3, nota4, nota5){
-//     let resultadoSuma = nota1 + nota2 + nota3 + nota4 + nota5;
-//     let promedio = resultadoSuma / 5;
-//     return promedio;
-
-// }
-
-
-// let nota1 = parseInt(prompt("ingrese la primer nota"));
-// let nota2 = parseInt(prompt("ingrese la segunda nota"));
-// let nota3 = parseInt(prompt("ingrese la tercera nota"));
-// let nota4 = parseInt(prompt("ingrese la cuarta nota"));
-// let nota5 = parseInt(prompt("ingrese la quinta nota"));
-
-// let respuesta = promedioNotas(nota1 , nota2 , nota3 , nota4 , nota5);
-// alert("TU PROMEDIO ES " + respuesta);
-
-// // meto las notas en un array // 
-
-// const arrayNotas = [];
-// arrayNotas.unshift(nota1, nota2, nota3, nota4 ,nota5);
-// console.log(arrayNotas);
-
-
-
-
-
-// creando eventos con imput // 
-
-const imput = document.getElementById('Nombre y Apellido')
-
-const email = document.getElementById('email')
-
-const tel = document.getElementById('phone')
-
-const descargo = document.getElementById('mensaje')
 
 const accion1 = () => {
-    console.log('Has hecho click')
+    formValues['Nombre'] = document.getElementById('validationCustom01').value;
+    localStorage.setItem('formValues' , JSON.stringify(formValues));
+    console.log(formValues);
+    console.log('Ingresando Nombre');
 }
 
 const accion2 = () => {
-    console.log('Ingresando mail')
+    formValues['Apellido'] = document.getElementById('validationCustom02').value;
+    localStorage.setItem('formValues' , JSON.stringify(formValues));
+    console.log(formValues);
+    console.log('Ingresando apellido')
 }
 
 const accion3 = () => {
-    console.log('discando numero')
+    formValues['Email'] = document.getElementById('validationCustomUsername').value;
+    localStorage.setItem('formValues' , JSON.stringify(formValues));
+    console.log(formValues);
+    console.log('ingresando mail')
 }
 
 const accion4 = () => {
-    console.log('dejando comentario')
+    formValues['Nombre'] = document.getElementById('validationCustom03').value;
+    localStorage.setItem('formValues' , JSON.stringify(formValues));
+    console.log(formValues);
+    console.log('ingresando ciudad')
 }
 
+    const accion5 = () => {
+        formValues['Nombre'] = document.getElementById('validationCustom05').value;
+        localStorage.setItem('formValues' , JSON.stringify(formValues));
+        console.log(formValues);
+        console.log('ingresando telefono')
+    }
 
-imput.addEventListener('click' , accion1)
 
-email.addEventListener('click' , accion2)
 
-tel.addEventListener('click' , accion3)
+Nombre.addEventListener('change' , accion1)
 
-descargo.addEventListener('click' , accion4)
+Apellido.addEventListener('change' , accion2)
+
+Email.addEventListener('change' , accion3)
+
+Ciudad.addEventListener('change' , accion4)
+
+Telefono.addEventListener('change' , accion5)
+
+
+
 
 
 
